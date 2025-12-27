caa894b63b42aed694ac13bd736e3f38250d9ac0
mikmak29 <mjogamaapple@gmail.com>
2025-12-27 22:45:36 +0800

    feat added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import asyncErrorHandler from "express-async-handler";
+import compression from "compression";
+import { rateLimit } from "express-rate-limit";
 import express from "express";
+
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/userDB.js";
 import { globalErrorHandler } from "./middleware/globalErrorHandler.js";
@@ -11,10 +14,20 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
+	standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+});
 
+app.use(compression());
+app.use(limiter);
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
 
 app.use("/api/user", userRoutes);
 
